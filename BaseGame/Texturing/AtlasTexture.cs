3c78d4d3092080cbcfe9bf023ffd911d2757756c
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2022-12-23 18:02:06 -0700

    Add Textures

---
+++ BaseGame/Texturing/AtlasTexture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CubivoxCore.BaseGame.Texturing
+{
+    public class AtlasTexture
+    {
+        public string location { private set; get; }
+        public int id { private set; get; }
+        public float xOffset { private set; get; }
+        public float yOffset { private set; get; }
+        
+        public AtlasTexture(string location)
+        {
+            this.location = location.Replace(".png", "");
+        }
+    }
+}
---
+++ BaseGame/Texturing/TextureAtlas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace CubivoxCore.BaseGame.Texturing
+{
+    public interface TextureAtlas
+    {
+        List<AtlasTexture> GetTextures();
+        int GetNumberOfRows();
+        float GetXOffset(int id);
+        float GetYOffset(int id);
+        void RecalculateTextureAtlas();
+        int GetTextureWidth();
+        int GetTextureHeight();
+        void SetTextureSize(int width, int height);
+        void RegisterTexture(AtlasTexture texture, bool recalculateAtlas);
+    }
+}
