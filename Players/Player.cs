d1d8278af8e520b6b31a75e06e970265ecce2df6
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2022-12-26 20:48:52 -0700

    Player Stuff

---
+++ Players/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CubivoxCore.Commands;
+using CubivoxCore.Permissions;
+using CubivoxCore.Worlds;
+
+namespace CubivoxCore.Players
+{
+    public interface Player : GameEntity, CommandSender, Permissible
+    {
+        Guid GetUUID();
+        string GetName();
+    }
+}
---
+++ Worlds/GameEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CubivoxCore;
+using CubivoxCore.BaseGame;
+
+namespace CubivoxCore.Worlds
+{
+    public interface GameEntity
+    {
+        Location GetLocation();
+        bool IsLiving();
+        Entity GetEntityType();
+    }
+}
