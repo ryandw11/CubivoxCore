c20bf3ea24877c111266824e6b57966cb5e29d4a
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2024-01-03 23:01:05 -0700

    Add Chat Color API

---
+++ Chat/RichStringExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubivoxCore.Chat
+{
+    public static class RichStringExtension
+    {
+        public static string Color(this string text, string color)
+        {
+            return $"<color = \"{color}\">{text}</color>";
+        }
+
+        public static string Italic(this string text)
+        {
+            return $"<i>{text}</i>";
+        }
+
+        public static string Bold(this string text)
+        {
+            return $"<b>{text}</b>";
+        }
+
+        public static string Underline(this string text)
+        {
+            return $"<u>{text}</u>";
+        }
+    }
+}
