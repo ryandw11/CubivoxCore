b1919e7da15ab1feead817c4809793dbd85bc11a
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2024-08-17 00:05:31 -0700

    Add CubivoxScheduler

--- Mods/ModDescriptionFile.cs
+++ Mods/ModDescriptionFile.cs
@@ -1,18 +1,15 @@
-﻿using System;
-
-namespace CubivoxCore.Mods
+﻿namespace CubivoxCore.Mods
 {
     /// <summary>
-    /// Represents the file that describes a mod. 
+    /// Represents the file that describes a mod.
     /// </summary>
-    [Serializable]
     public struct ModDescriptionFile
     {
 
-        public string ModName;
-        public string MainClass;
-        public string Description;
-        public string Version;
-        public string[] Authors;
+        public string ModName { get; set; }
+        public string MainClass { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        public string[] Authors { get; set; }
     }
 }
---
+++ Scheduler/CubivoxScheduler.cs
@@ -0,0 +1,49 @@
+﻿namespace CubivoxCore.Scheduler
+{
+    /// <summary>
+    /// Schedule tasks/actions to be executed on specific threads or at a specific point in time.
+    /// 
+    /// <para>This class is thread safe.</para>
+    /// </summary>
+    public abstract class CubivoxScheduler
+    {
+        protected static CubivoxScheduler mInstance;
+
+        public abstract void RunOnMainThreadImpl(Action action);
+        public abstract void RunOnMainThreadSynchronizedImpl(Action action);
+
+        /// <summary>
+        /// Execute an action on the main thread.
+        /// <para>On the client, the action will execute on the next frame.</para>
+        /// <para>On the server, the action will execute on the next tick.</para>
+        /// </summary>
+        /// <param name="action">The action to execute on the main thread.</param>
+        /// <exception cref="NotImplementedException">This will never occur on a normal installation of Cubivox.</exception>
+        public static void RunOnMainThread(Action action)
+        {
+            if(mInstance == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            mInstance.RunOnMainThreadImpl(action);
+        }
+
+        /// <summary>
+        /// If already on the main thread, execute the action in place immediately, else schedule it to be executed on the main thread.
+        /// <para>If not already on the main thread and on the client, the action will execute on the next frame.</para>
+        /// <para>If not already on the main thread and on the server, the action will execute on the next tick.</para>
+        /// </summary>
+        /// <param name="action">The action to execute on the main thread.</param>
+        /// <exception cref="NotImplementedException">This will never occur on a normal installation of Cubivox.</exception>
+        public static void RunOnMainThreadSynchronized(Action action)
+        {
+            if(mInstance == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            mInstance.RunOnMainThreadSynchronizedImpl(action);
+        }
+    }
+}
